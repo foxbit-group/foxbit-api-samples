e1aa59e69699aba30cd691dabef7981dcca4121f
Kevin Riquena <kevin.riquena@foxbit.com.br>
2025-05-06 08:35:58 -0300

    style: format code and improve readability in websocket-v2 example

--- websocket-v2/javascript/index.js
+++ websocket-v2/javascript/index.js
@@ -1,18 +1,17 @@
 // Instantiate Websocket module
 const WebSocket = require("ws");
-const crypto = require('crypto-js');
-const readline = require('readline');
+const crypto = require("crypto-js");
+const readline = require("readline");
 
 // Creating user interface
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 // WebSocketAPI setup
-const wsAddress = "";
+const wsAddress = "wss://api.foxbit.com.br/";
 const ws = new WebSocket(wsAddress);
-
 
 // Credentials
 const userId = "";
@@ -22,7 +21,9 @@ const apiSecret = "";
 // Authentication setup
 const nonce = Date.now();
 const signatureData = nonce + userId + apiKey;
-const signature = crypto.HmacSHA256(signatureData, apiSecret).toString(crypto.enc.Hex);
+const signature = crypto
+  .HmacSHA256(signatureData, apiSecret)
+  .toString(crypto.enc.Hex);
 
 // Message Frame
 const messageFrame = {
@@ -34,33 +35,40 @@ const messageFrame = {
 
 // Open connection to websocket
 ws.onopen = () => {
-  console.log("Connect to v2 Websocket!")
+  console.log("Connect to v2 Websocket!");
 
   // Send a message to AuthenticateUser
-  ws.send(JSON.stringify({
-    ...messageFrame,
-    n: "AuthenticateUser",
-    o: JSON.stringify({APIKey: apiKey, Nonce: nonce, UserId: userId, Signature: signature})
-  }));
+  ws.send(
+    JSON.stringify({
+      ...messageFrame,
+      n: "AuthenticateUser",
+      o: JSON.stringify({
+        APIKey: apiKey,
+        Nonce: nonce,
+        UserId: userId,
+        Signature: signature,
+      }),
+    })
+  );
 };
 
 // Logs the result of any message
-ws.on('message', (data) => {
+ws.on("message", (data) => {
   const result = JSON.parse(data);
   const resultFrame = result.n;
   const resultPayload = JSON.parse(result.o);
 
-  console.log({ resultFrame, resultPayload })
-  
-  if(resultFrame === "AuthenticateUser" && resultPayload.Authenticated) {
-    console.log("User authenticated:", resultPayload)
+  console.log({ resultFrame, resultPayload });
+
+  if (resultFrame === "AuthenticateUser" && resultPayload.Authenticated) {
+    console.log("User authenticated:", resultPayload);
     handleChannelChoice();
   }
 
-  if(resultFrame === "AuthenticateUser" && !resultPayload.Authenticated) {
+  if (resultFrame === "AuthenticateUser" && !resultPayload.Authenticated) {
     console.error("Authentication error: ", resultPayload);
 
-    setTimeout(function() {
+    setTimeout(function () {
       closeConn();
     }, 200);
   }
@@ -70,28 +78,31 @@ ws.on('message', (data) => {
 ws.onerror = (error) => {
   console.log("Error: " + JSON.stringify(error));
 
-  setTimeout(function() {
+  setTimeout(function () {
     closeConn();
-}, 200);
+  }, 200);
 };
 
 // Close connection
 ws.onclose = () => {
-  console.log("Connection closed!")
+  console.log("Connection closed!");
+  process.exit(0);
 };
 
 function closeConn() {
-  setTimeout(function() {
+  setTimeout(function () {
     ws.close();
-}, 200);
+  }, 200);
 }
 
 // User's interface
 function handleChannelChoice() {
-  rl.question('Select a channel to use (SubscribeLevel2, SubscribeAccountEvents): ', (channel) => {
-    console.log("Channel selected: ", channel);
+  rl.question(
+    "Select a channel to use (SubscribeLevel2, SubscribeAccountEvents): ",
+    (channel) => {
+      console.log("Channel selected: ", channel);
 
-      switch(channel) {
+      switch (channel) {
         case "SubscribeLevel2":
           subscribeLevel2();
           break;
@@ -104,28 +115,33 @@ function handleChannelChoice() {
           break;
       }
 
-    rl.close();
-  });
+      rl.close();
+    }
+  );
 }
 
 // Function to subscribe on the public channel SubscribeLevel2
 function subscribeLevel2() {
   console.log("SubscribeLevel2");
 
-   ws.send(JSON.stringify({
-    ...messageFrame,
-    n: 'SubscribeLevel2',
-    o: JSON.stringify({ OMSId: 1, MarketId: "btcbrl", Depth: 10 })
-  }));
+  ws.send(
+    JSON.stringify({
+      ...messageFrame,
+      n: "SubscribeLevel2",
+      o: JSON.stringify({ OMSId: 1, MarketId: "btcbrl", Depth: 10 }),
+    })
+  );
 }
 
 // Function to subscribe on the private channel SubscribeAccountEvents
 function subscribeAccountEvents() {
   console.log("SubscribeAccountEvents");
-  
-  ws.send(JSON.stringify({
-    ...messageFrame,
-    n: 'SubscribeAccountEvents',
-    o: JSON.stringify({})
-  }));
+
+  ws.send(
+    JSON.stringify({
+      ...messageFrame,
+      n: "SubscribeAccountEvents",
+      o: JSON.stringify({}),
+    })
+  );
 }
